7b93bc8ff04f17b482946e484868c9c3d6ea2876
Ronalds Sovas <sovas@greenbox.lv>
2020-12-03 14:11:25 +0200

    added details for student profile

--- source_code/web/resources/views/home.blade.php
+++ source_code/web/resources/views/home.blade.php
@@ -1,23 +1,38 @@
-@extends('layouts.app')
+@extends('layouts.public')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+<div class = "registration_container container">
+    <div class="jumbotron text-left">
+      <h2>Darba meklētāja profils</h2>
     </div>
-</div>
+  </div>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">      
+          <div class="btn-group-vertical">
+            <button type="button" class="btn btn-default" href="#">Mans profils</button>
+            <button type="button" class="btn btn-default" href="#">Mainīt datus</button>
+            <button type="button" class="btn btn-default" href="#">Mani pieteikumi</button>
+            <button type="button" class="btn btn-default" href="#">Darba pieredze</button>
+            <button type="button" class="btn btn-default" href="#">Saņemtās vēstules</button>
+            <button type="button" class="btn btn-default" href="#">Mani punkti</button>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+          <h3>Personīgie dati</h3>
+          <h4>{{ Auth::user()->name}}</h4> 
+          <h4>{{ Auth::user()->email}}</h4> 
+          <h4>26144428</h4> 
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+          <img src="http://via.placeholder.com/330x250" />
+          <button type="button" class="btn btn-default" href="#">Mainīt profila bildi</button>
+          <button type="button" class="btn btn-default" href="#">Pievienot CV</button>
+        </div>
+      </div>
+      <div class="alert alert-info">
+        <strong>Informācijai!</strong> Šī lapa ir tikai prototips.
+      </div>
+    </div>
 @endsection
